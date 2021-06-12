cf7d5a55d405c047caa8248cb424f897ef5301f0
Evy <evetricia.k@hotmail.fr>
2021-06-13 00:29:15 +0200

    routes ok

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth'); //on applique le middleware auth aux routes que l'on souhaite proteger
 const multer = require('../middleware/multer-config');
 
+const sauceCtrl = require('../controllers/sauce');
+
 /*routes sauces*/
 
-//route Post sauce et img
-router.post('/', auth, multer, sauceCtrl.createSauce);//multer est ajouté à la requete poste car on ajoute une photo a la creation de produit
+//route Post sauce qui capture et enregistre l'img, analyse la sauce et l'enregistre dans la bdd. remet les like et dislike à 0 et les tableaux userLiked et userDisliked vide
+//multer est ajouté à la requete poste car on ajoute une photo a la creation de produit
+router.post('/', auth, multer, sauceCtrl.createSauce);
 
-//route Post id sauce like
-router.post('/:id', auth, multer, sauceCtrl.likeOrDislikeSauce);
+//route Post id sauce like définit le statut j'aime pour l'userID fourni(1 = j'aime, 0 = annule, -1 = j'aime pas). L'id user doit être ajouté ou supprimé du tableau approprié et garder un trace de ce qu'il a aimé ou non en l'empêchant d'aime ou non plusieur fois la meme sauce. Une MAJ doit être faite a chaque j'aime
+router.post('/:id/like', auth, sauceCtrl.likeOrDislikeSauce);
 
-//route Put id sauce
-router.put('/:id', auth, sauceCtrl.modifySauce);
-
-//route delete id sauce
+//route delete id supprime la sauce avec l'id fourni 
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
-//route Get id sauce
+//route Put id MAJ la sauce avec l'id fourni, MAJ de l'image si new img, sinon maj des infos concernés(name, heat...)
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+
+//route Get sauce id qui renvoi la sauce avec l'id fourni
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 
-//route Get tableau sauces
+//route Get qui renvoi le tableau de toutes les sauces dans la base de donnée
 router.get('/', auth, sauceCtrl.getAllSauce);
 
 module.exports = router;
